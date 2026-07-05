fee4d64c69efafe6e1c58b6b460eadcdab775e37
Abdul-06-05 <abdulrahuman06062004@gmail.com>
2026-07-05 08:54:12 +0530

    table created

--- demo.sql
+++ demo.sql
@@ -11,3 +11,13 @@ where sal > 2000;
 
 select sal, sal*12 as annual_salary
 from emp;
+
+
+
+CREATE TABLE STUDENT
+(
+    STD_ID INT ,
+    STD_NAME VARCHAR(50),
+    AGE INT);
+INSERT INTO STUDENT VALUES(1006,'DEEPAN',21);
+SELECT * FROM STUDENT;
