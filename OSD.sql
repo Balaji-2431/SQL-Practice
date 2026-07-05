f8c1872163e2d00a01bd573e4871d2bd6d499483
Balaji-2431 <lovelybalaji597@gmail.com>
2026-07-05 08:49:56 +0530

    customer table created

--- OSD.sql
+++ OSD.sql
@@ -1,3 +1,5 @@
+-------------------------- TABLE DELETED ----------------------
+
 -- select * from tab;
 
 -- drop table orders;
@@ -11,4 +13,18 @@
 -- purge table ar;
 
 -- select * from tab;
+
+-------------------------- CREATE TABLES ----------------------
+
+create table Customers(
+    customer_id int primary key,
+    first_name varchar(50) not null,
+    last_name varchar(50) not null,
+    email varchar(100) unique not null,
+    phone varchar(15) not null,
+    city varchar(50) not null,
+    create_at date
+);
+
+insert into Customers values (2352,'Balaji','T','balaji@gmail.com','9345868996','Bodi',TO_DATE('05-JUL-2026','DD-MON-YYYY'));
 
