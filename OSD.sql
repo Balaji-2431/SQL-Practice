afbe89efb4b452fac673214b26b0e8b523714f55
Balaji-2431 <lovelybalaji597@gmail.com>
2026-07-05 08:31:29 +0530

    delete tables

---
+++ OSD.sql
@@ -0,0 +1,14 @@
+-- select * from tab;
+
+-- drop table orders;
+-- drop table customers;
+-- drop table pro;
+-- drop table ar;
+
+-- purge table customers;
+-- purge table orders;
+-- purge table pro;
+-- purge table ar;
+
+-- select * from tab;
+
